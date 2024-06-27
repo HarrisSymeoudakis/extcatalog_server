40a482fdef767797c3aa4d24151f4ad2b053af58
fedonch <fedonch@gmail.com>
2024-06-27 16:07:36 +0300

    Update extcatalog_server.mjs

--- extcatalog_server.mjs
+++ extcatalog_server.mjs
@@ -2,7 +2,7 @@ import axios from 'axios'; // Import Axios for making HTTP requests
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
-const fs = require('fs'); // File system module
+
 
 const app = express();
 const port = process.env.PORT || 1000;
