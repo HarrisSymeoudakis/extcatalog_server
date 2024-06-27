e44d7c3e24c560b520ed90654b309a2b7dd382ee
fedonch <fedonch@gmail.com>
2024-06-27 15:24:07 +0300

    Update extcatalog_server.mjs

--- extcatalog_server.mjs
+++ extcatalog_server.mjs
@@ -35,22 +35,41 @@ app.use((req, res, next) => {
 app.get('/items/getAllCatalog', async (req, res) => {
   try {
     
-    const url = baseUrl + jew1+jew2+jew3+wat1+wat2+wat3;
-	  // const url= "https://90571062-test-retail-ondemand.cegid.cloud/Y2/90571062_002_TEST/api/items/10AB0025%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X/images/v1";
+    // const url = baseUrl + jew1+jew2+jew3+wat1+wat2+wat3;
+	  const url= "https://90571062-test-retail-ondemand.cegid.cloud/Y2/90571062_002_TEST/api/items/10AB0025%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X/images/v1";
     // Make a GET request to the Swagger page with defined headers
-    const response = await axios.get(url, { headers });
+//     const response = await axios.get(url, { headers });
 
-    // Assuming the Swagger page returns JSON data
-    const swaggerData = response.data;
-    console.log('Received JSON data All catalog:', swaggerData);
+//     // Assuming the Swagger page returns JSON data
+//     const swaggerData = response.data;
+//     console.log('Received JSON data All catalog:', swaggerData);
 	
-    // Respond with the data received from the API
-    res.json(swaggerData);
+//     // Respond with the data received from the API
+//     res.json(swaggerData);
+//   } catch (error) {
+//     console.error('Error fetching data All catalog:', error);
+//     res.status(500).send('Error fetching data All catalog');
+//   }
+// });
+
+const response = await axios.get(url, { headers });
+
+    if (response.status === 200 && response.data.type === 'image/jpeg') {  // Check for successful response and image type
+      const blob = new Blob([response.data], { type: response.data.type });  // Create a Blob object
+      const objectURL = URL.createObjectURL(blob);  // Create an object URL
+
+      // Respond with the objectURL
+      res.json({ imageURL: objectURL });
+    } else {
+      console.error('Error fetching or invalid image data');
+      res.status(500).send('Error fetching image data');
+    }
   } catch (error) {
     console.error('Error fetching data All catalog:', error);
     res.status(500).send('Error fetching data All catalog');
   }
 });
+	  
 
 // Start the server
 app.listen(port, () => {
