e46119ad634fcd8fe2a0efc97352063f6a07adff
Charalampos Symeoudakis <CSymeoudakis@mspos.net>
2024-06-27 08:03:44 +0300

    Initial commit

---
+++ extcatalog_server.mjs
@@ -0,0 +1,49 @@
+import axios from 'axios'; // Import Axios for making HTTP requests
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 1000;
+
+// Encode username and password for Basic Authentication
+const username = '90478305_003_TEST\\AI';
+const password = '1234';
+const auth = Buffer.from(`${username}:${password}`).toString('base64');
+
+const headers = {
+  'Authorization': `Basic ${auth}`,
+  'Content-Type': 'application/json' // Adjust content type if needed
+};
+
+// Middleware to allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Update * to your specific origin if needed
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// All Customer Orders
+app.get('/items/getAllCatalog', async (req, res) => {
+  try {
+    
+    const url = 'https://90478305-partner-retail-ondemand.cegid.cloud/Y2/90478305_003_TEST/api/products-search/v1?ids=DE010009%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X&ids=DE010010%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X&ids=DE020002%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X&ids=DE020004%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X&fields=UserFields';
+
+    // Make a GET request to the Swagger page with defined headers
+    const response = await axios.get(url, { headers });
+
+    // Assuming the Swagger page returns JSON data
+    const swaggerData = response.data;
+    console.log('Received JSON data All catalog:', swaggerData);
+	
+    // Respond with the data received from the API
+    res.json(swaggerData);
+  } catch (error) {
+    console.error('Error fetching data All catalog:', error);
+    res.status(500).send('Error fetching data All catalog');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
