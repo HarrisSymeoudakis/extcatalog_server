770f7afc3cae4ea75d2ada565657cd747c3123ca
fedonch <fedonch@gmail.com>
2024-06-28 11:31:20 +0300

    Update extcatalog_server.mjs

--- extcatalog_server.mjs
+++ extcatalog_server.mjs
@@ -34,33 +34,59 @@ app.use((req, res, next) => {
 });
 
 //https://extcatalog-server.onrender.com/items/getAllCatalog
-// All Customer Orders
 app.get('/items/getAllCatalog', async (req, res) => {
-  try {
+
     
-  
-	  const url= "https://90571062-test-retail-ondemand.cegid.cloud/Y2/90571062_002_TEST/api/items/10AB0025%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X/images/v1";
+    const url = baseUrl + jew1+jew2+jew3+wat1+wat2+wat3;
+    // Define the headers for the request
+    const headers = {
+	  'Authorization': `Basic ${auth}`,
+	  'Content-Type': 'application/json' // Adjust content type if needed
+	};
 
-const response = await axios.get(url, { headers });
+    // Make a GET request to the Swagger page with defined headers
+    const response = await axios.get(url, { headers });
 
-    if (response.status === 200 ) {  // Check for successful response and image type
-      const blob = new Blob([response.data], { type: 'image/jpeg' });  // Create a Blob object
-	    console.log(blob);
-      // const objectURL = URL.createObjectURL(blob);  // Create an object URL
+    // Assuming the Swagger page returns JSON data
+    const swaggerData = response.data;
+    console.log('Received JSON data from Customer User fields:', swaggerData);
 
-      // Respond with the objectURL
-	    // console.log(objectURL);
-      // res.json({ imageURL: objectURL });
-	    res.json({ imageURL: blob });
-    } else {
-      console.error('Error fetching or invalid image data');
-      res.status(500).send('Error fetching image data');
-    }
+    // Respond with the data received from the API
+    res.json(swaggerData);
   } catch (error) {
-    console.error('Error fetching data All catalog:', error);
-    res.status(500).send('Error fetching data All catalog');
+    console.error('Error fetching data for customer User fields:', error);
+    res.status(500).send('Error fetching data for customer User fields');
   }
 });
+
+
+// All Customer Orders
+// app.get('/items/getAllCatalog', async (req, res) => {
+//   try {
+    
+  
+// 	  const url= "https://90571062-test-retail-ondemand.cegid.cloud/Y2/90571062_002_TEST/api/items/10AB0025%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X/images/v1";
+
+// const response = await axios.get(url, { headers });
+
+//     if (response.status === 200 ) {  // Check for successful response and image type
+//       const blob = new Blob([response.data], { type: 'image/jpeg' });  // Create a Blob object
+// 	    console.log(blob);
+//       // const objectURL = URL.createObjectURL(blob);  // Create an object URL
+
+//       // Respond with the objectURL
+// 	    // console.log(objectURL);
+//       // res.json({ imageURL: objectURL });
+// 	    res.json({ imageURL: blob });
+//     } else {
+//       console.error('Error fetching or invalid image data');
+//       res.status(500).send('Error fetching image data');
+//     }
+//   } catch (error) {
+//     console.error('Error fetching data All catalog:', error);
+//     res.status(500).send('Error fetching data All catalog');
+//   }
+// });
 	  
 
 // Start the server
