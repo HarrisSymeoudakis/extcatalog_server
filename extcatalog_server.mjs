ce3d429119d09c2d96e638a60b52ac79ca1e1844
fedonch <fedonch@gmail.com>
2024-06-28 10:22:30 +0300

    Update extcatalog_server.mjs

--- extcatalog_server.mjs
+++ extcatalog_server.mjs
@@ -24,6 +24,8 @@ const headers = {
   'Content-Type': 'application/json' // Adjust content type if needed
 };
 
+  // const url = baseUrl + jew1+jew2+jew3+wat1+wat2+wat3;
+
 // Middleware to allow CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Update * to your specific origin if needed
@@ -36,31 +38,14 @@ app.use((req, res, next) => {
 app.get('/items/getAllCatalog', async (req, res) => {
   try {
     
-    // const url = baseUrl + jew1+jew2+jew3+wat1+wat2+wat3;
+  
 	  const url= "https://90571062-test-retail-ondemand.cegid.cloud/Y2/90571062_002_TEST/api/items/10AB0025%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20X/images/v1";
-    
-//     const response = await axios.get(url, { headers });
-
-//     // Assuming the Swagger page returns JSON data
-//     const swaggerData = new Blob([response.data], { type: 'image/jpg' });  // Create a Blob object// response.data;
-//     console.log('Received JSON data All catalog:', swaggerData);
-// 	const objectURL = URL.createObjectURL(swaggerData); 
-//     // Respond with the data received from the API
-//     res.json(objectURL);
-//   } catch (error) {
-//     console.error('Error fetching data All catalog:', error);
-//     res.status(500).send('Error fetching data All catalog');
-//   }
-// });
-
-
-
-
 
 const response = await axios.get(url, { headers });
 
     if (response.status === 200 ) {  // Check for successful response and image type
       const blob = new Blob([response.data], { type: 'image/jpg' });  // Create a Blob object
+	    console.log(blob);
       const objectURL = URL.createObjectURL(blob);  // Create an object URL
 
       // Respond with the objectURL
